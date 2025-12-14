27fd10c4082710406842d8b3952a9a04fb3a533a
KHALED70000 <abdullah151337@gmail.com>
2025-12-14 20:41:29 +0600

    some Text Tyop fixed in footer section

--- src/Router/LayOuts/FixedLayOut/Footer.jsx
+++ src/Router/LayOuts/FixedLayOut/Footer.jsx
@@ -67,7 +67,7 @@ const Footer = () => {
             </div>
 
             <div className="border-t border-gray-700 mt-8 pt-4 text-center text-gray-500 text-sm">
-                © {new Date().getFullYear()} StitchAxis Pro. All rights reserved.
+                © {new Date().getFullYear()} NestCloth . All rights reserved.
             </div>
         </footer>
     );
